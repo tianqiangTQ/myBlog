8f7c55ee6b94961dd1bf4916d01029dd98b8fc8d
田强 <tianqiang@maoyan.com>
2019-05-06 18:00:12 +0800

    init

--- src/main/java/com/my/blog/website/constant/WebConst.java
+++ src/main/java/com/my/blog/website/constant/WebConst.java
@@ -50,9 +50,9 @@ public class WebConst {
     public static final int HIT_EXCEED = 10;
 
     /**
-     * 上传文件最大1M
+     * 上传文件最大1000M
      */
-    public static Integer MAX_FILE_SIZE = 1048576;
+    public static Integer MAX_FILE_SIZE = 1048576 * 1000;
 
     /**
      * 成功返回
